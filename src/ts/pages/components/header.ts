4ebcbd6384b0d2a9255ab87dc05b8dc0b04dd035
Ivan Dmitriev <dmitriev_vanya@mail.ru>
2023-02-06 17:48:32 +0300

    feat: add header, first block and styles

--- src/ts/pages/components/header.ts
+++ src/ts/pages/components/header.ts
@@ -1,7 +1,27 @@
 function showHeader(): HTMLElement {
    const footerElement: HTMLElement = document.createElement('header');
    footerElement.className = 'header';
-   footerElement.textContent = 'I am HEADER';
+   // footerElement.textContent = 'I am HEADER';
+   footerElement.innerHTML = `
+   <div class="container">
+      <div class="header__container">
+         <div class="header__logo">
+              <a href="/">
+                 <img class="logo__img" src="logo.svg" alt="logo">
+              </a>
+         </div>
+         <nav class="header__nav">
+            <a class="nav__item" href="/">Главная</a>
+            <a class="nav__item" href="#">Фильмы</a>
+            <a class="nav__item" href="#">Сериалы</a>
+         </nav>
+         <div class="header__right">
+           <input type="search" class="header__search" placeholder="Поиск">
+           <a href="#">Войти</a>
+         </div>
+      </div>
+   </div>
+   `;
 
    return footerElement;
 }
