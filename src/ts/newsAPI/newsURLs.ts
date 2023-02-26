c97d6a1338b9e8e06ae1123b6022746e5cdcf488
LetoCode <pavlov.egor@gmail.com>
2023-02-26 14:34:37 +0300

    fix: del console

--- src/ts/newsAPI/newsURLs.ts
+++ src/ts/newsAPI/newsURLs.ts
@@ -1,5 +1,5 @@
 export const NEWS_API_KEY = 'f6b3bf47a54f424392bb2b5058a1ff77';
-export const NEWS_DOMAIN = 'http://newsapi.org/v2/everything';
+export const NEWS_DOMAIN = 'https://newsapi.org/v2/everything';
 
 export const NEWS_REQUEST = (): string => {
    const query: URLSearchParams = new URLSearchParams();
