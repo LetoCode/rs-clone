3b378dc4291c30812716f1d57b50c4524809d42e
LetoCode <pavlov.egor@gmail.com>
2023-02-27 21:49:36 +0300

    fix: deploing changes

--- src/ts/imdbAPI/imdbURLs.ts
+++ src/ts/imdbAPI/imdbURLs.ts
@@ -3,13 +3,13 @@ export const IMDB_API_KEY = '8a44c7bc';
 export const IMDB_DOMAIN = (searchWord: string, year = ''): string => {
    if (year) {
       //for develop
-      return `http://www.omdbapi.com/?t=${searchWord}&y=${year}&apikey=${IMDB_API_KEY}&`
+      // return `http://www.omdbapi.com/?t=${searchWord}&y=${year}&apikey=${IMDB_API_KEY}&`
       //for deploy
-      //return `/omdbapi/?t=${searchWord}&y=${year}&apikey=${IMDB_API_KEY}&`
+      return `/omdbapi/?t=${searchWord}&y=${year}&apikey=${IMDB_API_KEY}&`
    }
    //for develop
-   return `http://www.omdbapi.com/?t=${searchWord}&apikey=${IMDB_API_KEY}&`
+   // return `http://www.omdbapi.com/?t=${searchWord}&apikey=${IMDB_API_KEY}&`
    //for deploy
-   //return `/omdbapi/?t=${searchWord}&apikey=${IMDB_API_KEY}&`
+   return `/omdbapi/?t=${searchWord}&apikey=${IMDB_API_KEY}&`
 };
 
--- src/ts/newsAPI/newsURLs.ts
+++ src/ts/newsAPI/newsURLs.ts
@@ -1,9 +1,9 @@
 export const NEWS_API_KEY = 'f6b3bf47a54f424392bb2b5058a1ff77';
 //for develop
-export const NEWS_DOMAIN = 'http://newsapi.org/v2/everything';
+// export const NEWS_DOMAIN = 'http://newsapi.org/v2/everything';
 
 //for deploy
-//export const NEWS_DOMAIN = '/newsapi'; 
+export const NEWS_DOMAIN = '/newsapi';
 
 export const NEWS_REQUEST = (): string => {
    const query: URLSearchParams = new URLSearchParams();
